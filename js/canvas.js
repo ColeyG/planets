283ab858513b10379980c01c1f3dcf561331817f
ColeyG <cjgeerts@gmail.com>
2017-12-26 19:17:11 -0500

    Adding vectored ship

--- js/canvas.js
+++ js/canvas.js
@@ -26,16 +26,18 @@ var asteroidSizeH=window.innerWidth/128;
 var shipSizeW=window.innerWidth/64;
 var shipSizeH=shipSizeW*1.75608756098;
 var angle=0;
+var moonRot=0;
 
 //asteroids
 var ax=Math.random()*innerWidth;
 var ay=Math.random()*innerHeight;
 var adx=Math.random()*10-5;
 var ady=Math.random()*10-5;
+var astDeg=0;
 
 //ship
 var sx=innerWidth/2-shipSizeW/2;
-var sy=innerHeight/2-shipSizeH/2;
+var sy=innerHeight/2-shipSizeH/2-earthSize/2-20;
 var shipSpeedX=0;
 var shipSpeedY=0;
 
@@ -58,6 +60,42 @@ for(var i=0;i<starAmount;i++){
   newStar();
 }
 
+//Rotate around center function
+function drawImageRot(img,x,y,width,height,deg){
+  var rad = deg * Math.PI / 180;
+  c.translate(x + width / 2, y + height / 2);
+  c.rotate(rad);   
+  c.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
+  c.rotate(rad * ( -1 ) );
+  c.translate((x + width / 2) * (-1), (y + height / 2) * (-1));
+}
+
+//Find ship rotation function
+function shipRot(){
+  var shypotenuse = (shipSpeedX*shipSpeedX)+(shipSpeedY*shipSpeedY);
+  var shypotenuse=Math.sqrt(shypotenuse);
+  var shangle=0;
+  if(shipSpeedX>0&&shipSpeedY<=0){
+    var shangle=Math.tan(shipSpeedX/shypotenuse)* (180 / Math.PI);
+  }
+  if(shipSpeedX<0&&shipSpeedY<=0){
+    var shangle=Math.tan(shipSpeedX/shypotenuse)* (180 / Math.PI);
+  }
+  if(shipSpeedX<0&&shipSpeedY>=0){
+    var shangle=Math.tan(shipSpeedX*(-1)/shypotenuse)* (180 / Math.PI);
+    shangle=shangle+180;
+  }
+  if(shipSpeedX>0&&shipSpeedY>=0){
+    var shangle=Math.tan(shipSpeedX*(-1)/shypotenuse)* (180 / Math.PI);
+    shangle=shangle+180;
+  }
+  if(shipSpeedX==0&&shipSpeedY>0){
+    shangle=180;
+  }
+  //SEND IFS TO CHECK QUADRANT
+  return shangle;
+}
+
 function animate(){
   requestAnimationFrame(animate);
   c.fillStyle="rgb(10,42,94)";
@@ -69,10 +107,13 @@ function animate(){
   //earth
   c.drawImage(earth,innerWidth/2-earthSize/2,innerHeight/2-earthSize/2,earthSize,earthSize);
   //moon
-  c.drawImage(moon,innerWidth/2-moonSize/2+Math.cos(angle)*radius,innerHeight/2-moonSize/2+Math.sin(angle)*radius,moonSize,moonSize);
+  //c.drawImage(moon,innerWidth/2-moonSize/2+Math.cos(angle)*radius,innerHeight/2-moonSize/2+Math.sin(angle)*radius,moonSize,moonSize);
+  drawImageRot(moon,innerWidth/2-moonSize+Math.cos(angle)*radius,innerHeight/2-moonSize/2+Math.sin(angle)*radius,moonSize,moonSize,moonRot);
   angle=angle+.01;
+  moonRot=moonRot+.6;
   //asteroid
-  c.drawImage(asteroid,ax,ay,asteroidSizeH*1.1484375,asteroidSizeH);
+  drawImageRot(asteroid,ax,ay,asteroidSizeH*1.1484375,asteroidSizeH,astDeg);
+  astDeg--;
   ax=ax+adx;
   ay=ay+ady;
   if (ax>innerWidth+asteroidSizeH+5){ax=0-asteroidSizeH;}
@@ -80,7 +121,8 @@ function animate(){
   if (ay>innerHeight+asteroidSizeH+5){ay=0-asteroidSizeH;}
   if (ay<0-asteroidSizeH-5){ay=innerHeight+asteroidSizeH;}
   //ship
-  c.drawImage(ship,sx,sy,shipSizeW,shipSizeH);
+  //c.drawImage(ship,sx,sy,shipSizeW,shipSizeH);
+  drawImageRot(ship,sx,sy,shipSizeW,shipSizeH,shipRot());
   sx=sx+shipSpeedX;
   sy=sy+shipSpeedY;
   if (sy<0-shipSizeH){sy=innerHeight+shipSizeH;}
@@ -102,14 +144,14 @@ function animate(){
 }
 
 document.addEventListener("keydown", function(event) {
-  console.log(event.which);
+  //console.log(event.which);
   if(event.which==87){w=true;}
   if(event.which==83){s=true;}
   if(event.which==65){a=true;}
   if(event.which==68){d=true;}
 });
 document.addEventListener("keyup", function(event) {
-  console.log('stop');
+  //console.log('stop');
   if(event.which==87){w=false;}
   if(event.which==83){s=false;}
   if(event.which==65){a=false;}
